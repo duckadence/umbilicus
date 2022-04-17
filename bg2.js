497e3c6fe26266e5ebef525cd2dcfe682d8ea13a
Jonathan Lin <jonlin11@gmail.com>
2022-04-17 02:47:45 +0000

    y'all told me to push

--- bg2.js
+++ bg2.js
@@ -1,26 +1,20 @@
-document.getElementById("timer").innerHTML = `
-<div class="base-timer">
-  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-    <g class="base-timer__circle">
-      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-      <path
-        id="base-timer-path-remaining"
-        stroke-dasharray="283"
-        class="base-timer__path-remaining ${remainingPathColor}"
-        d="
-          M 50, 50
-          m -45, 0
-          a 45,45 0 1,0 90,0
-          a 45,45 0 1,0 -90,0
-        "
-      ></path>
-    </g>
-  </svg>
-  <span id="base-timer-label" class="base-timer__label">${formatTime(
-    timeLeft
-  )}</span>
-</div>
-`;
+let TIME_LIMIT = 0;
+let timePassed;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
 
+chrome.runtime.onMessage.addListener()
 
-document.getElementById("button").addEventListener("click", setTime);
+function onTimesUp() {
+  clearInterval(timerInterval);
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+
+      onTimesUp();
+    }
+  }, 1000);
+}
